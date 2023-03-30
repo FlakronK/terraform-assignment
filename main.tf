5e4290134cb3d7e8e98acdecbc10d098f9e3a3cf
FlakronK <126613538+FlakronK@users.noreply.github.com>
2023-03-30 21:11:11 +0200

    ndryshim ne dev branch

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "terrform-assignment-flakron-bucket-${var.environment}"
-  acl    = "private"
   tags = {
     Environment = var.environment
   }
+}
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
 }
